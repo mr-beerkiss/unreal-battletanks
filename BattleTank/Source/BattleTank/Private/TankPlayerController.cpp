af2bb0a21e8ca25e8d729aeacf610eec8fa61952
Darren Beukes <code@darrenbeukes.com>
2020-06-23 23:05:04 +1000

    Line trace aiming

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,8 @@
 
 #include "TankPlayerController.h"
 
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
   return Cast<ATank>(GetPawn());
@@ -28,7 +30,81 @@ void ATankPlayerController::BeginPlay()
 void ATankPlayerController::Tick(float DeltaSeconds)
 {
   Super::Tick(DeltaSeconds);
-  // FString name = GetPawn()->GetName();
-  //
-  // UE_LOG(LogTemp, Warning, TEXT("Tick Pawn = %s"), *name);
+  AimTowardsCrossHair();
+}
+
+void ATankPlayerController::AimTowardsCrossHair() const
+{
+  if (!GetControlledTank())
+    return;
+
+  FVector HitLocation;
+
+  const bool bHitSomething = GetSightRayHitLocation(HitLocation);
+
+  // UE_LOG(LogTemp, Warning, TEXT("Player viewpoint = %s"), *HitLocation.ToString());
+
+  if (bHitSomething)
+  {
+    // Aim Barrel at Location of HitLocation
+  }
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
+{
+  
+  FVector PlayerViewPointLocation;
+  FRotator PlayerViewPointRotation;
+  this->GetPlayerViewPoint(PlayerViewPointLocation, PlayerViewPointRotation);
+    
+  // Find the location of the crosshair (it's in 2D space and in a Blueprint so that's weird)
+  // Need to "de-project" the screen position of cross hair to world position
+  int32 ViewportSizeX, ViewportSizeY;
+  GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+  // messy implicit type coercion :yuck:
+  const FVector2D ScreenLocation(CrossHairXLocation * ViewportSizeX, CrossHairYLocation * ViewportSizeY);
+
+  FVector WorldLocation, WorldDirection;
+
+  if (DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, WorldDirection))
+  {
+    // Line Trace through the cross from the POV of the player controller. Might need to offset the POV
+    // to from the edge of the barrel
+    // Test the line trace to see if it hits any actors (does the landscape count as an actor)
+    // Set a max range
+   if (GetLookVectorHitLocation(OutHitLocation, WorldDirection))
+   {
+     UE_LOG(LogTemp, Warning, TEXT("Got a hit location %s"), *OutHitLocation.ToString());
+     return true;
+   }
+  }
+  
+  return false;
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector &OutHitLocation, FVector LookDirection) const
+{
+  // Get the Camera Location
+  auto PlayerLocation = PlayerCameraManager->GetCameraLocation();
+  FVector LineTraceEnd = PlayerLocation + LookDirection * LineTraceRange;
+  FHitResult HitResult;
+
+  // DrawDebugLine(GetWorld(), PlayerLocation, LineTraceEnd, FColor(0, 255, 0), false, 0.f, 0, 5);
+  
+  FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+  
+  bool bHitSomething = GetWorld()->LineTraceSingleByChannel(
+    HitResult,
+    PlayerLocation,
+    LineTraceEnd,
+    ECC_Visibility,
+    TraceParams);
+  
+  if (bHitSomething)
+  {
+    OutHitLocation = HitResult.Location;
+  }
+  
+  return bHitSomething;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "Tank.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 #include "TankPlayerController.generated.h"
 
 /**
@@ -14,9 +16,21 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
   GENERATED_BODY()
+private:
+  UPROPERTY(EditAnywhere)
+  float CrossHairXLocation = .5;
+
+  UPROPERTY(EditAnywhere)
+  float CrossHairYLocation = .3333;
+
+  UPROPERTY(EditAnywhere)
+  float LineTraceRange = 1000000.0; // unreal units in cm
 public:
   ATank* GetControlledTank() const;
 
   virtual void BeginPlay() override;
   virtual void Tick(float DeltaSeconds) override;
+  void AimTowardsCrossHair() const;
+  bool GetSightRayHitLocation(FVector &OutHitLocation) const;
+  bool GetLookVectorHitLocation(FVector &OutHitLocation,  FVector LookDirection) const;
 };
