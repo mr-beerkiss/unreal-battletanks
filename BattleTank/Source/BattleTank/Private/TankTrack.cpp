64fbb49e4b22f800daaa0d1c06460318dac02ff0
Darren Beukes <code@darrenbeukes.com>
2020-07-13 22:03:13 +1000

    "Fixed" editor weirdness

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,5 @@
+// Copyright 2020 Darren Beukes
+
+
+#include "TankTrack.h"
+
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,17 @@
+// Copyright 2020 Darren Beukes
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
